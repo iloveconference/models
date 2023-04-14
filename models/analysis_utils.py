1bea943aa93ffdbe760ba02823ed5ff8e1bd56c2
Dallan Quass <dallan@quass.org>
2023-04-14 13:50:23 -0600

    analyze query logs

---
+++ models/analysis_utils.py
@@ -0,0 +1,64 @@
+"""Utility functions for analyzing logs."""
+
+import gzip
+import json
+import re
+from io import BytesIO
+from typing import Any
+
+import boto3
+from tqdm import tqdm
+
+
+def load_logs(bucket_name: str, prefix: str = "") -> list:
+    """Load logs from S3 bucket."""
+    # Initialize an empty list to hold the JSON objects
+    json_objects = []
+
+    # Create an S3 client
+    s3 = boto3.client("s3")
+
+    # Get a list of all the objects in the bucket with the specified prefix
+    objects = s3.list_objects_v2(Bucket=bucket_name, Prefix=prefix)["Contents"]
+
+    # Iterate over each object in the bucket
+    for obj in tqdm(objects):
+        # Download the object
+        file_obj = s3.get_object(Bucket=bucket_name, Key=obj["Key"])
+
+        # Check if the file is gzipped
+        if file_obj["ContentType"] in ["application/octet-stream", "application/x-gzip"]:
+            # Unzip the file
+            gzip_file = gzip.GzipFile(fileobj=BytesIO(file_obj["Body"].read()))
+            file_content = gzip_file.read().decode("utf-8")
+        else:
+            # Read the file content as plain text
+            file_content = file_obj["Body"].read().decode("utf-8")
+
+        # Parse the file content as JSON
+        # first, split separate JSON objects by adding a newline character
+        file_content = re.sub(r"}{\"(?![,}])", '}\n{"', file_content, flags=re.UNICODE)
+        # then parse each line as a JSON object
+        for line in file_content.split("\n"):
+            json_obj = json.loads(line)
+
+            # Add the JSON object to the list
+            json_objects.append(json_obj)
+
+    return json_objects
+
+
+def get_log_messages(logs: list[dict[str, Any]], message_type: str) -> list[dict, Any]:
+    """Get log messages of a specific type."""
+    messages = []
+    for log in logs:
+        if "logEvents" not in log:
+            continue
+        for log_event in log["logEvents"]:
+            if "message" not in log_event:
+                continue
+            message = json.loads(log_event["message"])
+            if "message" not in message or message["message"] != message_type:
+                continue
+            messages.append(message)
+    return messages
---
+++ tests/test_analysis_utils.py
@@ -0,0 +1,81 @@
+"""Test cases for the analysis_utils module."""
+
+from models import analysis_utils
+
+
+def test_get_log_messages():
+    """It returns log messages."""
+    log = {
+        "messageType": "DATA_MESSAGE",
+        "subscriptionFilters": ["server"],
+        "logEvents": [
+            {
+                "message": """{
+                "asctime": "2023-03-01 01:11:19,394",
+                "name": "server.main",
+                "levelname": "INFO",
+                "message": "search",
+                "prompt": "prompt",
+                "response": {
+                    "q": "question",
+                    "session": 3287431778,
+                    "answer": "answer.",
+                    "results": [{
+                        "id": 20635,
+                        "title": "title",
+                        "author": "author",
+                        "year": "2016",
+                        "month": "04",
+                        "url": "url",
+                        "text": "text."
+                    }, {
+                        "id": 88026,
+                        "title": "title2",
+                        "author": "author2",
+                        "year": "2014",
+                        "month": "10",
+                        "url": "url2",
+                        "text": "text2"
+                    }]
+                }
+            }""",
+            },
+            {
+                "message": """{
+                "asctime": "2023-03-01 01:11:19,394",
+                "name": "server.main",
+                "levelname": "INFO",
+                "message": "search",
+                "prompt": "prompt2",
+                "response": {
+                    "q": "question2",
+                    "session": 3287431778,
+                    "answer": "answer2.",
+                    "results": [{
+                        "id": 20635,
+                        "title": "title3",
+                        "author": "author3",
+                        "year": "2016",
+                        "month": "04",
+                        "url": "url3",
+                        "text": "text3."
+                    }, {
+                        "id": 88026,
+                        "title": "title4",
+                        "author": "author4",
+                        "year": "2014",
+                        "month": "10",
+                        "url": "url4",
+                        "text": "text4"
+                    }]
+                }
+            }""",
+            },
+        ],
+    }
+
+    log_messages = analysis_utils.get_log_messages([log], "search")
+    assert len(log_messages) == 2
+    assert log_messages[0]["prompt"] == "prompt"
+    assert log_messages[0]["response"]["q"] == "question"
+    assert log_messages[1]["prompt"] == "prompt2"
