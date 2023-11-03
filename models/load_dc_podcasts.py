49b40f088b2b7b9825ca268d0e3579438122cc85
Jigga Byte <jigga.e10@gmail.com>
2023-11-03 13:37:04 +0100

    added d&c podcasts loader notebook

--- models/load_dc_podcasts.py
+++ models/load_dc_podcasts.py
@@ -28,6 +28,8 @@ def load_dc_podcasts(url: str, html: str, bs_parser: str = "html.parser") -> Doc
 
     path_components = urlparse(url).path.split('/')
     title = path_components[-1]
+    title = title.replace('-',' ')
+    title = title.capitalize()
     content = clean(_to_markdown(html, base_url=url)) if html else ""
 
     metadata = {
@@ -35,6 +37,47 @@ def load_dc_podcasts(url: str, html: str, bs_parser: str = "html.parser") -> Doc
         "title": clean(title) if title else "",
     }
     return Document(page_content=content, metadata=metadata)
+
+
+def get_text_of_third_h2_in_second_div(html_content):
+    # Parse the HTML content using BeautifulSoup
+    soup = BeautifulSoup(html_content, 'html.parser')
+    # Find all div elements with class ‘elementor-col-50’
+    div_elements = soup.find_all('div', class_='elementor-col-50')
+    # Check if there is at least one div with the specified class
+    if len(div_elements) >= 2:
+        # Get the second div with class ‘elementor-col-50’
+        second_div = div_elements[1]
+        # Find all h2 elements within the second div
+        h2_elements = second_div.find_all('h2')
+        # Check if there is at least a third h2 element
+        if len(h2_elements) >= 3:
+            # Get the text of the third h2 element
+            third_h2_text = h2_elements[2].get_text()
+            return third_h2_text
+    # If the structure is not found, return None or an appropriate message
+    return None
+
+def get_html_of_second_section_under_eael_tabs_content(html_content):
+    # Parse the HTML content using BeautifulSoup
+    soup = BeautifulSoup(html_content, 'html.parser')
+    # Find all div elements with class ‘eael-tabs-content’
+    div_elements = soup.find_all('div', class_='eael-tabs-content')
+    # Check if there is at least one div with the specified class
+    if len(div_elements) >= 1:
+        # Get the first div with class ‘eael-tabs-content’
+        target_div = div_elements[0]
+        # Find all sections within the target div
+        section_elements = target_div.find_all('section')
+        # Check if there is at least a second section
+        if len(section_elements) >= 2:
+            # Get the second section
+            second_section = section_elements[1]
+            # Extract the HTML of the second section
+            second_section_html = str(second_section)
+            return second_section_html
+    # If the structure is not found, return None or an appropriate message
+    return None
 
 
 class PodcastsLoader(BaseLoader):
