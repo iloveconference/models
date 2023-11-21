3aacf7151eba520a6a83fec15dfef12942091cee
Elsie Essien <arikpoelsie02@gmail.com>
2023-11-21 14:44:11 +0100

    we reviewed the load_know

--- models/load_know.py
+++ models/load_know.py
@@ -1,10 +1,29 @@
 """Load knowhys."""
 
+import re
+
 from bs4 import BeautifulSoup
 from langchain.schema.document import Document
 
 from models.load_utils import clean
 from models.load_utils import to_markdown
+
+
+line = "* [1.](#footnoteref1"
+
+
+def remove_text_below_footnote(line, text):
+    """This function removes footnotes."""
+    pattern = re.compile(r"\* \[1\.\]\(#footnoteref1.*\)")
+    match = re.search(pattern, text)
+
+    if match:
+        # Remove everything after the matched line
+        cleaned_text = text[: match.start()]
+        return cleaned_text
+
+    # If no match is found, return the original text
+    return text
 
 
 def load_knowhy(url: str, html: str, bs_parser: str = "html.parser") -> Document:
@@ -16,6 +35,7 @@ def load_knowhy(url: str, html: str, bs_parser: str = "html.parser") -> Document
     citation = soup.find(id="block-views-knowhy-citation-block")
     body = soup.find("div", class_="group-left")
     content = clean(to_markdown(str(body), base_url=url)) if body else ""
+    clean_content = remove_text_below_footnote(line, content)
 
     metadata = {
         "url": url,
@@ -24,4 +44,4 @@ def load_knowhy(url: str, html: str, bs_parser: str = "html.parser") -> Document
         "date": clean(date) if date else "",
         "citation": clean(to_markdown(str(citation), base_url=url)) if citation else "",
     }
-    return Document(page_content=content, metadata=metadata)
+    return Document(page_content=clean_content, metadata=metadata)
